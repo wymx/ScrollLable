0c6c13af5eaa51e92f43825ee55efa2b9f26e15e
wymx <1029660589@qq.com>
2016-10-08 21:29:49 +0800

    Initial Commit

---
+++ ScrollLable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollLable
+//
+//  Created by WEI on 2016/10/8.
+//  Copyright © 2016年 weiyouming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollLable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollLable
+//
+//  Created by WEI on 2016/10/8.
+//  Copyright © 2016年 weiyouming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
